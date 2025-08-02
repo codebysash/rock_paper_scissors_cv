23cf72ed8e814488522050610eb9186bade399f0
sashshetty <95724242+codebysash@users.noreply.github.com>
2025-08-02 13:01:28 +1000

    Fixed timer and playerMove print logic to trigger once after countdown

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,17 @@
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
+import time
 
 cap = cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
 
 detector = HandDetector(maxHands=1)
+
+timer = 0
+stateResult = False
+startGame = False
 
 while True:
     imgBG = cv2.imread("Resources/BG.png")
@@ -18,12 +23,40 @@ while True:
     # Find Hands
     hands, img = detector.findHands(imgScaled)
 
+    if startGame:
+        if stateResult is False:
+            timer = time.time() - initialTime
+            cv2.putText(imgBG, str(int(timer)),(605,435), cv2.FONT_HERSHEY_PLAIN,6,(255,0,255), 4)
+
+            if timer>3:
+                stateResult = True
+                timer = 0
+
+                playerMove = 0 # Default value
+
+                if hands:
+                    hand = hands[0]
+                    fingers = detector.fingersUp(hand)
+                    if fingers == [0, 0, 0, 0, 0]:
+                        playerMove = 1
+                    elif fingers == [1, 1, 1, 1, 1]:
+                        playerMove = 2
+                    elif fingers == [0, 1, 1, 0, 0]:
+                        playerMove = 3
+
+                    print(playerMove)
+
+
+
     imgBG[234:654, 795:1195] = imgScaled
 
 
 
-    cv2.imshow("Image", img)
+    # cv2.imshow("Image", img)
     cv2.imshow("BG", imgBG)
-    cv2.imshow("Scaled", imgScaled)
+    # cv2.imshow("Scaled", imgScaled)
 
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        startGame = True
+        initialTime = time.time()
